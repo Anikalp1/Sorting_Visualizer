15216e0f8d33549bd859c28791acb4b07b0f16f1
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2024-06-08 21:52:12 +0530

    Implemented Audio API and fine-tuning

--- src/column.js
+++ src/column.js
@@ -1,72 +1,73 @@
-class Column {
-  constructor(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.queue = [];
+class Column{
+  constructor(x,y,width,height){
+      this.x=x;
+      this.y=y;
+      this.width=width;
+      this.height=height;
+      this.queue=[];
+      this.color={
+          r:150,
+          g:150,
+          b:150
+      }
   }
 
-  moveTo(loc, yOffset = 1, frameCount = 20) {
-    for (let i = 1; i <= frameCount; i++) {
-      const t = i / frameCount;
-      const u = Math.sin(t * Math.PI);
-      this.queue.push({
-        x: lerp(this.x, loc.x, t),
-        y: lerp(this.y, loc.y, t) + ((u * this.width) / 2) * yOffset,
-      });
-    }
+  moveTo(loc,yOffset=1,frameCount=5){
+      for(let i=1;i<=frameCount;i++){
+          const t=i/frameCount;
+          const u=Math.sin(t*Math.PI);
+          this.queue.push({
+              x:lerp(this.x,loc.x,t),
+              y:lerp(this.y,loc.y,t)+
+                  u*this.width/2*yOffset,
+              r:lerp(150,255,u),
+              g:lerp(150,0,u),
+              b:lerp(150,0,u)
+          });
+      }
   }
 
-  jump(frameCount = 20) {
-    for (let i = 1; i < frameCount; i++) {
-      const t = i / frameCount;
-      const u = Math.sin(t * Math.PI);
-      this.queue.push({
-        x: this.x,
-        y: this.y - u * this.width,
-      });
-    }
+  jump(frameCount=5){
+      for(let i=1;i<=frameCount;i++){
+          const t=i/frameCount;
+          const u=Math.sin(t*Math.PI);
+          this.queue.push({
+              x:this.x,
+              y:this.y-u*this.width,
+              r:lerp(150,100,u),
+              g:lerp(150,100,u),
+              b:lerp(150,100,u)
+          });
+      }
   }
-  draw(ctx) {
-    let changed = false;
-    if (this.queue.length > 0) {
-      const { x, y } = this.queue.shift();
-      this.x = x;
-      this.y = y;
-      changed = true;
-    }
-    const left = this.x - this.width / 2;
-    const top = this.y - this.height;
-    const right = this.x + this.width / 2;
 
-    ctx.beginPath();
-    ctx.fillStyle = "rgb(150, 150, 150)";
-    ctx.moveTo(left, top);
-    ctx.lineTo(left, this.y);
-    ctx.ellipse(
-      this.x,
-      this.y,
-      this.width / 2,
-      this.width / 4,
-      0,
-      Math.PI,
-      Math.PI * 2,
-      true
-    );
-    ctx.lineTo(right, top);
-    ctx.ellipse(
-      this.x,
-      top,
-      this.width / 2,
-      this.width / 4,
-      0,
-      0,
-      Math.PI * 2,
-      true
-    );
-    ctx.fill();
-    ctx.stroke();
-    return changed;
+  draw(ctx){
+      let changed=false;
+      if(this.queue.length>0){
+          const {x,y,r,g,b}=this.queue.shift();
+          this.x=x;
+          this.y=y;
+          this.color={r,g,b};
+          changed=true;
+      }
+      const left=this.x-this.width/2;
+      const top=this.y-this.height;
+      const right=this.x+this.width/2;
+
+      ctx.beginPath();
+      const {r,g,b}=this.color;
+      ctx.fillStyle=`rgb(${r},${g},${b})`;
+      ctx.moveTo(left,top);
+      ctx.lineTo(left,this.y);
+      ctx.ellipse(this.x,this.y,
+          this.width/2,this.width/4,0,
+          Math.PI,Math.PI*2,true);
+      ctx.lineTo(right,top);
+      ctx.ellipse(this.x,top,
+          this.width/2,this.width/4,0,
+          0,Math.PI*2,true);
+      ctx.fill();
+      ctx.stroke();
+      return changed;
   }
 }
--- src/script.js
+++ src/script.js
@@ -1,72 +1,105 @@
-myCanvas.width = 400;
-myCanvas.height = 300;
+myCanvas.width=500;
+myCanvas.height=400;
+const margin=30;
+const n=30;
+const array=[];
+let moves=[];
+const cols=[];
+const spacing=(myCanvas.width-margin*2)/n;
+const ctx=myCanvas.getContext("2d");
 
-const margin = 30;
-const n = 20;
-const array = [];
-let moves = [];
-const cols = [];
-const spacing = (myCanvas.width - margin * 2) / n;
-const ctx = myCanvas.getContext("2d");
-const maxColumnHeight = 200;
+const maxColumnHeight=200;
 
 init();
 
-function init() {
-  for (let i = 0; i < n; i++) {
-    array[i] = Math.random();
-  }
-  moves = [];
-  for (let i = 0; i < array.length; i++) {
-    const x = i * spacing + spacing / 2 + margin;
-    const y = myCanvas.height - margin - i * 3;
-    const width = spacing - 4;
-    const height = maxColumnHeight * array[i];
+let audioCtx=null;
 
-    cols[i] = new Column(x, y, width, height);
-  }
+function playNote(freq,type){
+    if(audioCtx==null){
+        audioCtx=new(
+            AudioContext || 
+            webkitAudioContext || 
+            window.webkitAudioContext
+        )();
+    }
+    const dur=0.2;
+    const osc=audioCtx.createOscillator();
+    osc.frequency.value=freq;
+    osc.start();
+    osc.type=type;
+    osc.stop(audioCtx.currentTime+dur);
+
+    const node=audioCtx.createGain();
+    node.gain.value=0.4;
+    node.gain.linearRampToValueAtTime(
+        0, audioCtx.currentTime+dur
+    );
+    osc.connect(node);
+    node.connect(audioCtx.destination);
 }
 
-function play() {
-  moves = bubbbleSort(array);
+function init(){
+    for(let i=0;i<n;i++){
+        array[i]=Math.random();
+    }
+    moves=[];
+    for(let i=0;i<array.length;i++){
+        const x=i*spacing+spacing/2+margin;
+        const y=myCanvas.height-margin-i*3;
+        const width=spacing-4;
+        const height=maxColumnHeight*array[i];
+        cols[i]=new Column(x,y,width,height);
+    }
+}
+
+function play(){
+    moves=bubbleSort(array);
 }
 
 animate();
 
-function bubbbleSort(array) {
-  const moves = [];
-  do {
-    var swapped = false;
-    for (let i = 1; i < array.length; i++) {
-      if (array[i - 1] > array[i]) {
-        swapped = true;
-        [array[i - 1], array[i]] = [array[i], array[i - 1]];
-        moves.push({ indices: [i - 1, i], swap: true });
-      } else {
-        moves.push({ indices: [i - 1 - i], swap: false });
-      }
-    }
-  } while (swapped);
-  return moves;
+function bubbleSort(array){
+    const moves=[];
+    do{
+        var swapped=false;
+        for(let i=1;i<array.length;i++){
+            if(array[i-1]>array[i]){
+                swapped=true;
+                [array[i-1],array[i]]=[array[i],array[i-1]];
+                moves.push(
+                    {indices:[i-1,i],swap:true}
+                );
+            }else{
+                moves.push(
+                    {indices:[i-1,i],swap:false}
+                );
+            }
+        }
+    }while(swapped);
+    return moves;
 }
-function animate() {
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  let changed = false;
-  for (let i = 0; i < cols.length; i++) {
-    changed = cols[i].draw(ctx) || changed;
-  }
 
-  if (!changed && moves.length > 0) {
-    const move = moves.shift();
-    const [i, j] = move.indices;
-    if (move.swap) {
-      cols[i].moveTo(cols[j]);
-      cols[j].moveTo(cols[i], -1);
-      [cols[i], cols[j]] = [cols[j], cols[i]];
-    } else {
-      cols[i].jump();
-      cols[j].jump();
+function animate(){
+    ctx.clearRect(0,0,myCanvas.width,myCanvas.height);
+    let changed=false;
+    for(let i=0;i<cols.length;i++){
+        changed=cols[i].draw(ctx)||changed;
     }
-  }
-  requestAnimationFrame(animate);
+
+    if(!changed && moves.length>0){
+        const move=moves.shift();
+        const [i,j]=move.indices;
+        const waveformType=move.swap?"square":"sine";
+        playNote(cols[i].height+cols[j].height,waveformType);
+        if(move.swap){
+            cols[i].moveTo(cols[j]);
+            cols[j].moveTo(cols[i],-1);
+            [cols[i],cols[j]]=[cols[j],cols[i]];
+        }else{
+            cols[i].jump();
+            cols[j].jump();
+        }
+    }
+
+    requestAnimationFrame(animate);
 }
