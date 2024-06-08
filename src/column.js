60b79dbb8fab30429280c6cca9d7546481156795
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2024-06-08 20:34:21 +0530

    Implemented sorting technique

--- src/column.js
+++ src/column.js
@@ -1,46 +1,62 @@
-class Column{
-    constructor(x, y, width, height)
-    {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.queue = [];
-    }
+class Column {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.queue = [];
+  }
 
-    moveTo(loc, frameCount = 100)
-    {
-        for(let i = 1; i<=frameCount; i++)
-            {
-                const t = i/frameCount;
-                this.queue.push(
-                    {
-                        x:lerp(this.x, loc.x, t),
-                        y:lerp(this.y, loc.y, t)
-                    }
-                );
-            }
+  moveTo(loc, yOffset = 1, frameCount = 20) {
+    for (let i = 1; i <= frameCount; i++) {
+      const t = i / frameCount;
+      const u = Math.sin(t * Math.PI);
+      this.queue.push({
+        x: lerp(this.x, loc.x, t),
+        y: lerp(this.y, loc.y, t) + (u * this.width) / 2 * yOffset,
+      });
     }
+  }
 
-    draw(ctx){
-        if(this.queue.length>0)
-            {
-                const {x, y} = this.queue.shift();
-                this.x = x;
-                this.y = y;
-            }
-        const left = this.x - this.width/2;
-        const top =  this.y - this.height;
-        const right = this.x + this.width/2;
-
-        ctx.beginPath();
-        ctx.fillStyle = "rgb(150, 150, 150)";
-        ctx.moveTo(left, top);
-        ctx.lineTo(left, this.y);
-        ctx.ellipse(this.x, this.y, this.width/2, this.width/4, 0, Math.PI, Math.PI*2, true);
-        ctx.lineTo(right, top);
-        ctx.ellipse(this.x, top, this.width/2, this.width/4, 0, 0, Math.PI*2, true);
-        ctx.fill();
-        ctx.stroke();
+  draw(ctx) {
+    let changed = false;
+    if (this.queue.length > 0) {
+      const { x, y } = this.queue.shift();
+      this.x = x;
+      this.y = y;
+      changed = true;
     }
+    const left = this.x - this.width / 2;
+    const top = this.y - this.height;
+    const right = this.x + this.width / 2;
+
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(150, 150, 150)";
+    ctx.moveTo(left, top);
+    ctx.lineTo(left, this.y);
+    ctx.ellipse(
+      this.x,
+      this.y,
+      this.width / 2,
+      this.width / 4,
+      0,
+      Math.PI,
+      Math.PI * 2,
+      true
+    );
+    ctx.lineTo(right, top);
+    ctx.ellipse(
+      this.x,
+      top,
+      this.width / 2,
+      this.width / 4,
+      0,
+      0,
+      Math.PI * 2,
+      true
+    );
+    ctx.fill();
+    ctx.stroke();
+    return changed;
+  }
 }
--- src/script.js
+++ src/script.js
@@ -24,13 +24,47 @@ for (let i = 0; i < array.length; i++) {
   cols[i] = new Column(x, y, width, height);
 }
 
+let moves = bubbbleSort(array);
+
 animate();
 
+function bubbbleSort(array) {
+  const moves = [];
+  do {
+    var swapped = false;
+    for (let i = 1; i < array.length; i++) {
+      if (array[i - 1] > array[i]) {
+        swapped = true;
+        [array[i - 1], array[i]] = [array[i], array[i - 1]];
+        moves.push({ indices: [i - 1, i], swap: true });
+      } else {
+        moves.push({ indices: [i - 1 - i], swap: false });
+      }
+    }
+  } while (swapped);
+  return moves;
+}
 function animate() {
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  let changed = false;
   for (let i = 0; i < cols.length; i++) {
-    cols[i].draw(ctx);
+    changed = cols[i].draw(ctx) || changed;
   }
 
+  if(!changed && moves.length>0)
+    {
+        const move = moves.shift();
+        const [i, j] = move.indices;
+        if(move.swap)
+            {
+                cols[i].moveTo(cols[j]);
+                cols[j].moveTo(cols[i], -1);
+                [cols[i], cols[j]] = [cols[j], cols[i]];
+            }
+        else
+        {
+            //to-do
+        }
+    }
   requestAnimationFrame(animate);
 }
